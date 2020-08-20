03904ef4a619b32258c16d1ef603c6e696f53515
Lukas Gölles <goelles.lukas@gmail.com>
2020-08-20 16:22:46 +0200

    autoload video

--- dist/audio.js
+++ dist/audio.js
@@ -69,6 +69,8 @@ function isMobile() {
     var height = videoElement.offsetHeight;
     player.width(width), player.height(height);
     player.muted(false);
+    video.preload = 'auto';
+    video.autoload = true;
     player.panorama({
         clickToToggle: (!isMobile()),
         clickAndDrag: true,
