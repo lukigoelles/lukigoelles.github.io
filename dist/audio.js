a79366a8301ff848a716953932d1022b894dd095
Lukas Gölles <goelles.lukas@gmail.com>
2020-08-24 09:55:01 +0200

    new permission

--- dist/audio.js
+++ dist/audio.js
@@ -1,5 +1,35 @@
 "use strict";
 
+window.onload = function () {
+
+    // Check if is IOS 13 when page loads.
+    if ( window.DeviceMotionEvent && typeof window.DeviceMotionEvent.requestPermission === 'function' ){
+  
+        // Everything here is just a lazy banner. You can do the banner your way.
+        const banner = document.createElement('div')
+        banner.innerHTML = `<div style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Click here to enable DeviceMotion</p></div>`
+        banner.onclick = ClickRequestDeviceMotionEvent // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
+        document.querySelector('body').appendChild(banner)
+    }
+  }
+  
+  
+  function ClickRequestDeviceMotionEvent () {
+    window.DeviceMotionEvent.requestPermission()
+      .then(response => {
+        if (response === 'granted') {
+          window.addEventListener('devicemotion',
+            () => { console.log('DeviceMotion permissions granted.') },
+            (e) => { throw e }
+        )} else {
+          console.log('DeviceMotion permissions not granted.')
+        }
+      })
+      .catch(e => {
+        console.error(e)
+      })
+  }
+
 const IR_PATH = './decodingFilters/';
 var decodingFiltersLoaded = false;
 const SPATIALIZATION_UPDATE_MS = 25;
