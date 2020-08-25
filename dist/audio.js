bb7d2b54e7abe59c5f6881aef661f05a8e5b93f5
Lukas Gölles <goelles.lukas@gmail.com>
2020-08-25 11:21:37 +0200

    some improvements

--- dist/audio.js
+++ dist/audio.js
@@ -5,18 +5,18 @@ var GainNodes = [];
 var sourceNodesObjects = [];
 var a = [];
 
-window.onload = function () {
+// window.onload = function () {
 
-    // Check if is IOS 13 when page loads.
-    if ( window.DeviceMotionEvent && typeof window.DeviceMotionEvent.requestPermission === 'function' ){
+//     // Check if is IOS 13 when page loads.
+//     if ( window.DeviceMotionEvent && typeof window.DeviceMotionEvent.requestPermission === 'function' ){
   
-        // Everything here is just a lazy banner. You can do the banner your way.
-        const banner = document.createElement('div')
-        banner.innerHTML = `<div style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Click here to enable DeviceMotion</p></div>`
-        banner.onclick = ClickRequestDeviceMotionEvent // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
-        document.querySelector('body').appendChild(banner)
-    }
-  }
+//         // Everything here is just a lazy banner. You can do the banner your way.
+//         const banner = document.createElement('div')
+//         banner.innerHTML = `<div style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Click here to enable DeviceMotion</p></div>`
+//         banner.onclick = ClickRequestDeviceMotionEvent // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
+//         document.querySelector('body').appendChild(banner)
+//     }
+//   }
   
   
   function ClickRequestDeviceMotionEvent () {
@@ -68,9 +68,11 @@ if (isMobile() && this.audioElement.canPlayType('audio/ogg; codecs="opus"') === 
     }
 }
 
- if (normalAudio) {
+ if (true) {
     const soundEffect = new Audio();
     const soundEffect2 = new Audio();
+    soundEffect.src = './assets/' + videoToLoad + 'WY.mp3';
+    soundEffect2.src = './assets/' + videoToLoad + 'ZX.mp3';
     var AudioContext = window.AudioContext || window.webkitAudioContext;
     this.context = new AudioContext;
     console.log(this.context);
@@ -82,34 +84,31 @@ if (isMobile() && this.audioElement.canPlayType('audio/ogg; codecs="opus"') === 
         const events = ['touchstart','touchend', 'mousedown','keydown'];
         events.forEach(e => b.addEventListener(e, unlock, false));
         function unlock() { audioCtx.resume().then(clean); }
-        function clean() { events.forEach(e => b.removeEventListener(e, unlock)); console.log(audioCtx.state); }
+        function clean() { events.forEach(e => b.removeEventListener(e, unlock)); console.log(audioCtx.state); ClickRequestDeviceMotionEvent;}
       }
 
     var allAudio = true;
-    soundEffect.src = './assets/' + videoToLoad + 'WY.mp3';
-    soundEffect2.src = './assets/' + videoToLoad + 'ZX.mp3';
-    soundEffect.load();
+    //soundEffect.load();
     //soundEffect2.load();
     // soundEffect2.src = './assets/' + videoToLoad + '90.mp3';
     // const audio1 = new Audio();
     // audio1.src = './assets/' + videoToLoad + '.mp3';
-    this.splitter = context.createChannelSplitter(4);
+    // this.splitter = context.createChannelSplitter(4);
     this.merger = context.createChannelMerger(4);
     
-    //this.audioNode = context.createMediaElementSource(soundEffect);
-    // this.audioNode2 = context.createMediaElementSource(soundEffect2);
+     this.audioNode = context.createMediaElementSource(soundEffect);
+    this.audioNode2 = context.createMediaElementSource(soundEffect2);
 
-    // this.audioNode.channelCount = 4;
-    // this.audioNode2.channelCount = 4;
+    this.audioNode.channelCount = 4;
+    this.audioNode2.channelCount = 4;
 
     // this.context.createGain();
     // console.log(this.context.state); // running
 
     var tapped = function() {
         if(allAudio) {
-            soundEffect.play();
-            soundEffect.pause();
-            soundEffect.currentTime = 0;
+            soundEffect.load();
+            soundEffect2.load();
             allAudio = false;
             console.log('AudioContext playback resumed successfully');
         }
@@ -121,7 +120,7 @@ if (isMobile() && this.audioElement.canPlayType('audio/ogg; codecs="opus"') === 
     // //     });
     // // });
 
-    // document.body.addEventListener('touchstart', tapped, false);
+     document.body.addEventListener('touchstart', tapped, false);
 
     this.order = 1;
 
@@ -140,20 +139,20 @@ if (isMobile() && this.audioElement.canPlayType('audio/ogg; codecs="opus"') === 
     // this.rotator.out.connect(this.decoder.in);
     // this.decoder.out.connect(context.destination);
 
-    // this.audioNode.connect(this.merger,0,0);
-    // this.audioNode.connect(this.merger,0,1);
-    // this.audioNode2.connect(this.merger,0,2);
-    // this.audioNode2.connect(this.merger,0,3);
+    this.audioNode.connect(this.merger,0,0);
+    this.audioNode.connect(this.merger,0,1);
+    this.audioNode2.connect(this.merger,0,2);
+    this.audioNode2.connect(this.merger,0,3);
 
-    // this.merger.connect(this.rotator.in)
-    // this.rotator.out.connect(this.decoder.in);
-    // this.decoder.out.connect(context.destination);
+    this.merger.connect(this.rotator.in)
+    this.rotator.out.connect(this.decoder.in);
+    this.decoder.out.connect(context.destination);
     //this.audioNode.connect(context.destination);
 
     player.on("play", function () {
         console.log("Play");
         soundEffect.play();
-        //soundEffect2.play();
+        soundEffect2.play();
     });
 
     player.on("pause", function () {
@@ -179,7 +178,7 @@ if (isMobile() && this.audioElement.canPlayType('audio/ogg; codecs="opus"') === 
         rotator.pitch = PHI * 180. / Math.PI -90;
         rotator.updateRotMtx();
         let currentTime = player.currentTime();
-        if(currentTime > 0 && !update){
+        if(currentTime > 1 && !update){
             soundEffect.currentTime = currentTime;
             soundEffect2.currentTime = currentTime;
             console.log('Update proceeded!');
