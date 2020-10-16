9761ba362180264707c4c0bfdacefd9afa04cca6
Lukas Gölles <goelles.lukas@gmail.com>
2020-10-16 18:23:33 +0200

    update

--- dist/audio.js
+++ dist/audio.js
@@ -590,7 +590,7 @@ if (isMobile() && this.audioElement.canPlayType('audio/ogg; codecs="opus"') === 
     setInterval(function() {
         delay = player.currentTime()-audioPlayer.getVideoElement().currentTime;
         if(synccounter < 10){
-            if((!isSync && player.currentTime() > 0 || Math.abs(player.currentTime()-audioPlayer.getVideoElement().currentTime)>0.07) && audioElementsObjects.isReady() && movie.readyState() == 4){
+            if((!isSync && player.currentTime() > 0 || Math.abs(player.currentTime()-audioPlayer.getVideoElement().currentTime)>0.07) && audioPlayer..isReady() && movie.readyState() == 4){
                 audioPlayer.getVideoElement().currentTime = audioPlayer.getVideoElement().currentTime+delay;
                 console.log('Sync!');
                 isSync = true;
